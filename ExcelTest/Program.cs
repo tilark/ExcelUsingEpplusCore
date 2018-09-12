a2d64c7219c566c6ad7bc72d22727502fd6a5733
tilark <tilark.liu@gmail.com>
2018-09-12 19:13:46 +0800

    添加测试项目
    1. 测试了TestExcelToDicitonary，通过

---
+++ ExcelTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+namespace ExcelTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestExcelToDicitonary();
+        }
+        static void TestExcelToDicitonary()
+        {
+            using (var file = File.Open("e:/test.xlsx", FileMode.Open))
+            {
+                var readExcel = ExcelWithEpplusCore.ExcelEntityFactory.GetInstance().CreateReadFromExcel();
+                var readResult = readExcel.ExcelToDicitonary(file);
+                int key = 1;
+
+                foreach (var firstDic in readResult)
+                {
+                    Console.WriteLine("Key:" + key);
+                    Console.WriteLine(firstDic.Key);
+                    foreach(var secondDic in firstDic.Value)
+                    {
+                        Console.WriteLine("Key: " + secondDic.Key + ", " + "Value: " + secondDic.Value);
+
+                    }
+                }
+            }
+        }
+    }
+}
